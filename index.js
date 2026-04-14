7e3e424cca992a3cb589bf468349905fa2e3fdaa
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-14 19:11:04 +0000

    sdrftg

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 class TaxCalculator{
     
     constructor(){
-        this.rate = 15;
+        this.#rate = 15;
         this.state = 'TX';
         this.exempt = false;
-    }
     
-    calculate(){
+        calculate(){
         if(this.exempt){
             return calculateExempt(1.37);
         }else{
             return calculateNonExempt(5.72);
         }
     }
+}
+    
+   
 }
 
 //should hide the rate
